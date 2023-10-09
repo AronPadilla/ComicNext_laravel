9a1b1226a82aebc5bef701c37be22acd49dfd904
rolando <rolamdomr@gmail.com>
2023-10-09 16:13:53 -0400

    Subida de imagenes por registro

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\Comic;
+use App\Models\comic;
 use App\Models\Comic_categoria;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
