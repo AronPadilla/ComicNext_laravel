2a2cb0c360a7eea95640ed7ab03ca911811d2f82
AronPadilla <aronpad123@gmail.com>
2023-10-20 00:30:53 -0600

    Crear playlist register

---
+++ app/Http/Controllers/CrearPlaylistController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Playlist;
+use Illuminate\Http\Request;
+
+class CrearPlaylistController extends Controller
+{
+    public function register(Request $request){
+         $imagen_playlist = $request->imagen_playlist;
+        $playlist = new Playlist();
+        $playlist ->nombre_playlist = $request -> titulo;
+        $playlist ->imagen_playlist = str_replace("''", "'", pg_escape_bytea(base64_decode($imagen_playlist))); 
+        $playlist ->save();
+    }
+    
+}
