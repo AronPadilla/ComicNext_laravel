6cbee35b1c68f329562556ff1c0858e9d74a0b34
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2023-10-20 18:09:29 -0400

    Registro de comic a playlist

--- app/Http/Controllers/ComicPlaylistController.php
+++ app/Http/Controllers/ComicPlaylistController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+use App\Models\Comic_playlist;
 
 class ComicPlaylistController extends Controller
 {
-    //
+    public function registrarComicAPlaylist(Request $request)
+    {
+        //
+        DB::beginTransaction(); // Iniciar una transacción en la base de datos
+
+        try {
+            // Registrar el cómic
+            $comicPlaylist = new Comic_playlist();
+            $comicPlaylist->cod_comic = $request -> cod_comic;
+            $comicPlaylist->cod_usuario = $request -> cod_uduario;
+            $comicPlaylist->cod_playlist = $request -> cod_playlist;
+           
+            $comicPlaylist->save();
+
+            DB::commit(); // Confirmar la transacción si todo se realizó con éxito
+
+            return response()->json(['mensaje' => 'Cómic registrado a playlist con éxito']);
+
+        } catch (\Exception $e) {
+            DB::rollback(); // Revertir la transacción en caso de error
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 }
