ca4428a2339af914d74a792891093aa5ccf22b60
Teresavz <202103856@est.umss.edu>
2023-10-25 01:02:35 -0400

    CorreosExistentes

--- app/Http/Controllers/RegistroUsuarioController.php
+++ app/Http/Controllers/RegistroUsuarioController.php
@@ -4,41 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\Usuario;
 use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 
 class RegistroUsuarioController extends Controller
 {
     public function registrar(Request $request)
     {
-        // Validar los datos del formulario (debes personalizar estas validaciones)
-       // $request->validate([
-         //   'name' => 'required',
-           // 'username' => 'required|unique:usuario',
-            //'email' => 'required|email|unique:usuario',
-            //'password' => 'required|min:6',
-       // ]
+        $correo = $request->input('email');
 
-        // Crear un nuevo usuario en la base de datos 
+        // Verifica si el correo ya está registrado
+        $usuarioExistente = DB::table('usuario')->where('correo', $correo)->exists();
+
+        if ($usuarioExistente) {
+            return response()->json(['message' => 'Correo ya registrado']);
+        }
+
+        // Si el correo no está registrado, procede con el registro del usuario
         $usuario = new Usuario();
-        $usuario->nombre_completo = $request->name;
-        $usuario->nombre_u = $request->username;
-        $usuario->correo = $request->email;
-        $usuario->password = bcrypt($request->password); // Hash de la contraseña
-        $usuario->cod_rol = 2; 
+        $usuario->nombre_completo = $request->input('name');
+        $usuario->nombre_u = $request->input('username');
+        $usuario->correo = $correo;
+        $usuario->password = bcrypt($request->input('password'));
+        $usuario->cod_rol = 2;
 
         $usuario->save();
 
-        // Respuesta de éxito
-        return response()->json(['message' => 'Usuario registrado con éxito']);
+        return response()->json(['message' => 'El usuario ha sido registrado con éxito']);
     }
- //   public function CorreoExistente($correo)
-// {
-//     $usuario = DB::table('user')->where('correo', $correo)->first();
-//     if (!$usuario){
-//         return response ()->json(['exists'=>false]);
-
-//     }else{
-//         return response()->json(['exist'=>true]);
-//     }
-//}
 }
