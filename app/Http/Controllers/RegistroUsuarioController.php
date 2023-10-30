062ca5c6f001a346979644441b45b0e5b935ca0c
Teresavz <202103856@est.umss.edu>
2023-10-29 21:10:11 -0400

    Restriccion nombre de usuario

--- app/Http/Controllers/RegistroUsuarioController.php
+++ app/Http/Controllers/RegistroUsuarioController.php
@@ -8,20 +8,33 @@ class RegistroUsuarioController extends Controller
     public function registrar(Request $request)
     {
         $correo = $request->input('email');
+        $nombreUsuario = $request->input('username');
+    
         // Verifica si el correo ya está registrado
-        $usuarioExistente = DB::table('usuario')->where('correo', $correo)->exists();
-        if ($usuarioExistente) {
-            return response()->json(['message' => 'Correo ya registrado']);
+        $correoExistente = DB::table('usuario')->where('correo', $correo)->exists();
+        
+        // Verifica si el nombre de usuario ya está registrado
+        $nombreUsuarioExistente = DB::table('usuario')->where('nombre_u', $nombreUsuario)->exists();
+        
+        if ($correoExistente && $nombreUsuarioExistente) {
+            return response()->json(['message' => 'Correo y nombre de usuario ya están registrados en esta página']);
+        } elseif ($correoExistente) {
+            return response()->json(['message' => 'Correo ya está registrado en esta página']);
+        } elseif ($nombreUsuarioExistente) {
+            return response()->json(['message' => 'Nombre de usuario ya está registrado en esta página']);
         }
-
-        // Si el correo no está registrado, procede con el registro del usuario
+    
+        // Si ni el correo ni el nombre de usuario están registrados, procede con el registro del usuario
         $usuario = new Usuario();
         $usuario->nombre_completo = $request->input('name');
-        $usuario->nombre_u = $request->input('username');
+        $usuario->nombre_u = $nombreUsuario;
         $usuario->correo = $correo;
         $usuario->password = bcrypt($request->input('password'));
         $usuario->cod_rol = 2;
         $usuario->save();
+        
         return response()->json(['message' => 'El usuario ha sido registrado con éxito']);
     }
+    
+    
 }
