b7af1ba64df1a61c946ac94a0f554fb962a59094
rolando <rolamdomr@gmail.com>
2023-11-04 23:02:53 -0400

    Mandar emails desde laravel

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,13 @@ Route::get('/', function () {
 //Route::get('/RegistroComics',[RegistroController::class, 'register']);
 Route::post('/', [RegistroController::class, 'register'])->name('computer');
 
+Route::get('send-mail', function () {
+    $details = [
+        'title' => 'Success',
+        'content' => 'This is an email testing using Laravel-Brevo',
+    ];
+   
+    \Mail::to('202103856@est.umss.edu')->send(new \App\Mail\TestMail($details));
+   
+    return 'Email sent at ' . now();
+});
