d814a0201dd2bfd2fa3af57748279d137d43ee59
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2023-11-06 01:02:07 -0400

    Metodos para bloquear usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Usuario;
+use App\Models\UsuarioBloqueado;
 
 class UserController extends Controller
 {
@@ -22,10 +23,45 @@ class UserController extends Controller
     }
 
     public function incrementarFallidos(Request $request, $username){
-        //$username = $request->input('nombre_u');
-
         Usuario::where('nombre_u', $username)->increment('nro_fallidos');
         $nuevoNroFallidos = Usuario::where('nombre_u', $username)->value('nro_fallidos');
         return $nuevoNroFallidos;
+    }
+
+    public function verificarCuentaBloqueada($idUser){
+        $user = UsuarioBloqueado::where('cod_usuario', $idUser)->first();
+        if($user){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function bloquearCuenta(Request $request, $idUser){
+        try{
+            $creacionTime = now();
+
+            $usuarioBloqueado = new UsuarioBloqueado();
+            $usuarioBloqueado->cod_usuario = $idUser;
+            $usuarioBloqueado->inicio_bloqueo = $creacionTime;
+            $usuarioBloqueado->fin_bloque = $creacionTime->addHour();
+
+            return response()->json(['mensaje' => 'Usuario bloqueado']);
+        } catch (\Exception $e) {
+            
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function desbloquearCuenta(Request $request, $idUser){
+        $usuarioBloqueado = UsuarioBloqueado::where('cod_usuario', $idUser)->first();
+
+        if ($usuarioBloqueado) {
+            // Si se encuentra una fila con el ID de usuario, eliminarla
+            $usuarioBloqueado->delete();
+            return response()->json(['mensaje' => 'Usuario desbloqueado']);
+        } else {
+            return response()->json(['error' => 'Usuario no encontrado'], 401);
+        }
     }
 }
--- app/Models/UsuarioBloqueado.php
+++ app/Models/UsuarioBloqueado.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class UsuarioBloqueado extends Model
 {
     use HasFactory;
+    protected $table = 'usuario_bloqueado'; 
+
+    protected $fillable = ['cod_usuario','inicio_bloqueo','fin_bloqueo'];
+    public $timestamps = false;
+
+    protected $primaryKey = 'cod_bloqueado';
+    public $incrementing = true;
 }
