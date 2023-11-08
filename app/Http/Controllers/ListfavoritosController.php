8d133910461f0c53843f0d7f314fe6061e2fbe41
AronPadilla <aronpad123@gmail.com>
2023-11-08 01:09:13 -0600

    listfavoritos controller

--- app/Http/Controllers/ListfavoritosController.php
+++ app/Http/Controllers/ListfavoritosController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Me_gusta;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class listfavoritosController extends Controller
 {
-    //
+    public function registroComicFavoritos(Request $request)
+    {
+    
+        DB::beginTransaction();
+    
+        try {
+            // Crea una nueva instancia del modelo Me_gusta y asigna los valores
+            $comic_favoritos = new Me_gusta();
+            $comic_favoritos->cod_comic = $request->cod_comic;
+            $comic_favoritos->cod_usuario = $request->cod_usuario;
+            $comic_favoritos->fecha_creacion = now();
+            
+            $comic_favoritos->save();
+    
+            DB::commit();
+    
+            return response()->json(['mensaje' => 'Cómic registrado a playlist con éxito'], 200);
+        } catch (\Exception $e) {
+            DB::rollback(); // Revertir la transacción en caso de error
+            return response()->json(['error' => $e->getMessage()], 500);
+        } 
+    }
+     
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ContenidoController;
 use App\Models\Contenido;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\listfavoritosController;
 use App\Http\Controllers\ResetPasswordController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -86,7 +87,7 @@ Route::match(['get', 'post'], '/registro', [RegistroController::class, 'register
 
 Route::match(['get', 'post'], '/registroplay', [CrearPlaylistController::class, 'registro']);
 Route::get('/listasPlaylist/{idUsuario}',[CrearPlaylistController::class, 'getPlaylist']);
-Route::match(['get', 'post','delete'],'/deleteComicPlaylist',[ComicPlaylistController::class, 'destroy']); 
+Route::match(['get', 'post','delete'],'/deleteComicPlaylist',[ComicPlaylistController::class, 'destroy']);
 //Route::get('/registro-usuario', 'RegistroUsuarioController@index')->name('registro-usuario.index');
 //Route::post('/registro-usuario', 'RegistroUsuarioController@registrar')->name('registro-usuario.registrar');
 Route::match(['get', 'post'], '/registro-usuario', [RegistroUsuarioController::class, 'registrar']);
