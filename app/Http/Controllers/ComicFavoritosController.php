717ad1e0c110d56f90cd32f92666b99eaa4ec3eb
AronPadilla <aronpad123@gmail.com>
2023-11-11 18:11:39 -0600

    registro y delete en comic favoritos

--- app/Http/Controllers/ComicFavoritosController.php
+++ app/Http/Controllers/ComicFavoritosController.php
@@ -8,26 +8,36 @@ use Illuminate\Http\Request;
 
 class ComicFavoritosController extends Controller
 {
-    public function registroComicFavoritos(Request $request)
+    public function registroComicFavoritosLike(Request $request)
     {
-    
         DB::beginTransaction();
-    
+
         try {
-            // Crea una nueva instancia del modelo Me_gusta y asigna los valores
-            $comic_favoritos = new Me_gusta();
-            $comic_favoritos->cod_comic = $request->cod_comic;
-            $comic_favoritos->cod_usuario = $request->cod_usuario;
-            $comic_favoritos->fecha_creacion = now();
-            
-            $comic_favoritos->save();
-    
-            DB::commit();
-    
-            return response()->json(['mensaje' => 'Cómic registrado a playlist con éxito'], 200);
+            // Busca si ya existe una entrada en la lista de favoritos para el cómic y usuario especificados
+            $comic_favoritos = Me_gusta::where('cod_comic', $request->cod_comic)
+                                    ->where('cod_usuario', $request->cod_usuario)
+                                    ->get();
+
+            if (count($comic_favoritos) ) {
+                // Si ya existe una entrada, elimínala (quitar de favoritos)
+                Me_gusta::where('cod_comic', $request->cod_comic)
+                        ->where('cod_usuario', $request->cod_usuario)
+                        ->delete();
+                DB::commit();
+                return response()->json(['mensaje' => 'Comic eliminado'], 200);
+            } else {
+                // Si no existe una entrada, crea una nueva (agregar a favoritos)
+                $nuevoComicFavorito = new Me_gusta();
+                $nuevoComicFavorito->cod_comic = $request->cod_comic;
+                $nuevoComicFavorito->cod_usuario = $request->cod_usuario;
+                $nuevoComicFavorito->fecha_creacion = now();
+                $nuevoComicFavorito->save();
+                DB::commit();
+                return response()->json(['mensaje' => 'Cómic añadido a la lista de favoritos con éxito'], 200);
+            }
         } catch (\Exception $e) {
             DB::rollback(); // Revertir la transacción en caso de error
             return response()->json(['error' => $e->getMessage()], 500);
-        } 
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -89,8 +89,7 @@ Route::match(['get', 'post'], '/registro', [RegistroController::class, 'register
 Route::match(['get', 'post'], '/registroplay', [CrearPlaylistController::class, 'registro']);
 Route::get('/listasPlaylist/{idUsuario}',[CrearPlaylistController::class, 'getPlaylist']);
 Route::match(['get', 'post','delete'],'/deleteComicPlaylist',[ComicPlaylistController::class, 'destroy']);
-// Route::match(['get', 'post'], '/ComicFavoritos', [ListfavoritosController::class, 'registroComicFavoritos']);
-Route::match(['get', 'post'], '/ComicFavoritos', [ComicFavoritosController::class, 'registroComicFavoritos']);
+Route::match(['get', 'post','delete'], '/ComicFavoritosLike', [ComicFavoritosController::class, 'registroComicFavoritosLike']);
 //Route::get('/registro-usuario', 'RegistroUsuarioController@index')->name('registro-usuario.index');
 //Route::post('/registro-usuario', 'RegistroUsuarioController@registrar')->name('registro-usuario.registrar');
 Route::match(['get', 'post'], '/registro-usuario', [RegistroUsuarioController::class, 'registrar']);
