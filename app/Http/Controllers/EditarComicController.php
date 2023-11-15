f7737265690fb49448d18041ed0f5ce9af9c0ef7
rolando <rolamdomr@gmail.com>
2023-11-15 17:53:13 -0400

    Cambiar categorias en editar comic

--- app/Http/Controllers/EditarComicController.php
+++ app/Http/Controllers/EditarComicController.php
@@ -36,13 +36,55 @@ class EditarComicController extends Controller
             // Maneja las categorías según la estructura de tu base de datos
         ]);
 
-        // Buscar el cómic por ID
-        $categorias = Comic_categoria::where('cod_comic', $id);
+        // Eliminar todas las ctegorias el cómic por ID
+        $cats = Comic_categoria::where('cod_comic', $id)->delete();
+
+        // Obtener el ID del cómic registrado
+        $codigoComic = $id;
+        $categorias = $request->categoria;
+        $codCategorias = array(); 
 
         foreach ($categorias as $categoria) {
-            $categoria->delete();
+            switch ($categoria) {
+                case "Terror":
+                    $codCategorias[] = 1; 
+                    break;
+                case "CienciaFiccion":
+                    $codCategorias[] = 3;
+                    break;
+                case "Comedia":
+                    $codCategorias[] = 4;
+                    break;
+                case "Accion":
+                    $codCategorias[] = 2;
+                    break;
+                default:
+                    echo "Opción no válida";
+                    break;
+            }
         }
+
+// Ahora $codCategorias contiene los códigos numéricos correspondientes a las categorías.
+
+        
+        // $codigosCategoria = $request->input('codigosCategoria');
+
+        if (is_array($codCategorias)) {
+             foreach ($codCategorias as $codCategoria) {
+                 // Asociar el cómic con las categorías seleccionadas
+                 Comic_categoria::create([
+                     'cod_comic' => $codigoComic,
+                     'cod_categoria' => $codCategoria,
+                ]);
+            }
+        } else {
+             // Manejar un error si $codigosCategoria no es un array
+             throw new \Exception('Los códigos de categoría no son válidos.');
+         }
 
         return response()->json(['message' => 'Cómic editado exitosamente']);
     }
+
+  
+    
 }
